961fb358ac091696e397c150adb989566e22fa1e
Merci <160075925+mercaguiat@users.noreply.github.com>
2025-09-17 01:38:56 +0800

    #55. Deriving & Outputting Data Based on State

--- 01-starting-project/src/data.js
+++ 01-starting-project/src/data.js
@@ -25,3 +25,52 @@ export const CORE_CONCEPTS = [
         description: "A way to manage the state of a component. It is the source of truth."
     }
 ];
+
+export const EXAMPLES = {
+    components: {
+        title: 'Components',
+        description: 
+            'Components are the building blocks of React apps. They are self-contained, reusable pieces of UI.',
+        code: `
+        function Welcome(){
+            return (
+                <h1>Hello, world!</h1>;
+            )
+        }
+        `,
+    },
+    jsx: {
+        title: 'JSX',
+        description: 
+            'JSX is a syntax extension for JavaScript. It allows you to write HTML in JavaScript.',
+        code: `
+       <div> 
+            <h1>Hello, {userName}!</h1>;
+            <p>This is a paragraph.</p>;
+       </div> 
+        `,
+    },
+    props: {
+        title: 'Props',
+        description: 
+            'Props are a way to pass data from a parent component to a child component. They are read-only.',
+        code: `
+        function Welcome(props) {
+            return (
+                <h1>Hello, {props.userName}!</h1>;
+            )
+        }`,
+    },
+    state: {
+        title: 'State Management',
+        description: 
+            'State management is a way to manage the state of a component. It is the source of truth.',
+        code: `
+        function Welcome() {
+            const [userName, setUserName] = useState('');
+            return (
+                <h1>Hello, {userName}!</h1>;
+            )
+        }`,
+    }   
+}
